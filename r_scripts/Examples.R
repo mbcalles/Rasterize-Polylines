b4928be9f8b21a6ada3aae14f0295c26e3f37cf3
m.b-calles <mbcalles@uvic.ca>
2019-04-07 12:41:11 -0700

    Create markdown hmtl from R Script

--- r_scripts/Examples.R
+++ r_scripts/Examples.R
@@ -16,7 +16,15 @@ sep_bls <- st_read("input_data",
                    layer = "sep_bls")#separated bike lane polylines
 
 
-ggplot(bound) + geom_map
+ggplot(bound) +
+  geom_sf() + 
+  ggtitle("City of Vancouver Boundary overlaid by separated bike lane polylines") + 
+  geom_sf(data = sep_bls,color="black") + 
+  xlab("Longitude") + 
+  ylab("Latitude") +
+  theme_bw()
+
+
 
 #"Rasterize" polyline data using custom function
 
@@ -25,10 +33,22 @@ sepbls_rast <- StudyAreaLineDensityRaster(inputStudyArea = VanCT,
                                           cellSize = 100,
                                           buffWidth = 100)
 
-plot(sepbls_rast)
 
 
-sepbls_rast_mask <- mask(sepbls_rast, as(bound, "Spatial"))
+
+sepbls_rast_df <- as.data.frame(sepbls_rast, xy = TRUE)
+
+
+ggplot() +
+  geom_raster(data = sepbls_rast_df,aes(x=x,y=y,fill=layer)) + 
+  scale_fill_viridis_c() +
+  ggtitle("City of Vancouver Bikelane Density") + 
+  xlab("Longitude") + 
+  ylab("Latitude") +
+  coord_quickmap() + 
+  theme_bw()
+
+# sepbls_rast_mask <- mask(sepbls_rast, as(bound, "Spatial"))
 
 
 # writeRaster(x = sepbls_rast_mask,
